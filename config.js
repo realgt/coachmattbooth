48e4bf0274758c4d316cea3578a914bf3210cb16
Fabio Rodriguez <realgt@gmail.com>
2021-09-14 10:29:28 -0400

    updated menu, pages

--- config.js
+++ config.js
@@ -1,39 +1,32 @@
 module.exports = {
-  url: 'https://gatsby-starter-blog-and-cv.netlify.app',
-  pathPrefix: '/',
-  title: 'Blog by Marat Dospolov',
-  keywords:
-    'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
-  subtitle: 'Developer. Optimizer. Learner.',
-  copyright: '© 2020 | Kyiv, Ukraine',
-  disqusShortname: 'dospolov',
-  postsPerPage: 20,
-  googleAnalyticsId: 'UA-6589522-7',
-  menu: [
-    {
-      label: 'Blog',
-      path: '/'
-    },
-    {
-      label: 'CV',
-      path: '/cv'
+    url: 'https://gatsby-starter-blog-and-cv.netlify.app',
+    pathPrefix: '/',
+    title: 'Blog by Coach Matt Booth',
+    keywords: 'coaching, mentoring, coach matt booth, matt booth',
+    subtitle: 'Empowering our youth to write Extraordinary Stories that will Last a Lifetime.',
+    copyright: '© 2021',
+    disqusShortname: 'coachmattbooth',
+    postsPerPage: 20,
+    googleAnalyticsId: 'G-WWMVMRXHLV',
+    menu: [{
+        label: 'Blog',
+        path: '/'
+    }],
+    author: {
+        name: 'Coach Matt Booth',
+        photo: '/photo.jpg',
+        bio: 'Empowering our youth to write Extraordinary Stories that will Last a Lifetime.',
+        contacts: {
+            // don't remove fields, just make them empty string ''
+            // https://github.com/gatsbyjs/gatsby/issues/2392
+            github: '',
+            twitter: 'CoachMattBooth',
+            linkedin: '',
+            telegram: '',
+            instagram: '',
+            facebook: '',
+            email: '',
+            rss: '/rss.xml'
+        }
     }
-  ],
-  author: {
-    name: 'Marat Dospolov',
-    photo: '/photo.jpg',
-    bio: 'Developer. Optimizer. Learner.',
-    contacts: {
-      // don't remove fields, just make them empty string ''
-      // https://github.com/gatsbyjs/gatsby/issues/2392
-      github: 'dospolov',
-      twitter: 'marat_dospolov',
-      linkedin: 'dospolov',
-      telegram: 'dospolov',
-      instagram: 'dospolov',
-      facebook: 'dospolov',
-      email: '',
-      rss: '/rss.xml'
-    }
-  }
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,42 @@
 const siteConfig = require('./config.js')
 
 module.exports = {
-  pathPrefix: siteConfig.pathPrefix,
-  siteMetadata: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    keywords: siteConfig.keywords,
-    subtitle: siteConfig.subtitle,
-    copyright: siteConfig.copyright,
-    disqusShortname: siteConfig.disqusShortname,
-    menu: siteConfig.menu,
-    author: siteConfig.author
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content`,
-        name: 'pages'
-      }
+    pathPrefix: siteConfig.pathPrefix,
+    siteMetadata: {
+        url: siteConfig.url,
+        title: siteConfig.title,
+        keywords: siteConfig.keywords,
+        subtitle: siteConfig.subtitle,
+        copyright: siteConfig.copyright,
+        disqusShortname: siteConfig.disqusShortname,
+        menu: siteConfig.menu,
+        author: siteConfig.author
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/media`,
-        name: 'media'
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'assets',
-        path: `${__dirname}/static`
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
+    plugins: [{
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/content`,
+                name: 'pages'
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/static/media`,
+                name: 'media'
+            }
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'assets',
+                path: `${__dirname}/static`
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-feed',
+            options: {
+                query: `
           {
             site {
               siteMetadata {
@@ -48,19 +47,18 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) =>
-              allMarkdownRemark.edges.map(edge =>
-                Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.description,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.site_url + edge.node.fields.slug,
-                  guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }]
-                })
-              ),
-            query: `
+                feeds: [{
+                    serialize: ({ query: { site, allMarkdownRemark } }) =>
+                        allMarkdownRemark.edges.map(edge =>
+                            Object.assign({}, edge.node.frontmatter, {
+                                description: edge.node.frontmatter.description,
+                                date: edge.node.frontmatter.date,
+                                url: site.siteMetadata.site_url + edge.node.fields.slug,
+                                guid: site.siteMetadata.site_url + edge.node.fields.slug,
+                                custom_elements: [{ 'content:encoded': edge.node.html }]
+                            })
+                        ),
+                    query: `
               {
                 allMarkdownRemark(
                   limit: 1000,
@@ -85,65 +83,63 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
-            title: 'Blog by Marat Dospolov'
-          }
-        ]
-      }
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: { maxWidth: 960 }
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
-          },
-          'gatsby-remark-autolink-headers',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          {
-            resolve: 'gatsby-remark-video',
-            options: {
-              width: 600,
-              height: 'auto',
-              preload: 'auto',
-              muted: true,
-              autoplay: true,
-              playsinline: true,
-              controls: false,
-              loop: true
+                    output: '/rss.xml',
+                    title: 'Blog by Coach Matt Booth'
+                }]
             }
-          }
-        ]
-      }
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-netlify',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/index.js`
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-gtag',
-      options: {
-        trackingIds: [siteConfig.googleAnalyticsId],
-        pluginConfig: {
-          head: true
-        }
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-sitemap',
-      options: {
-        query: `
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [{
+                        resolve: 'gatsby-remark-images',
+                        options: { maxWidth: 960 }
+                    },
+                    {
+                        resolve: 'gatsby-remark-responsive-iframe',
+                        options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
+                    },
+                    'gatsby-remark-autolink-headers',
+                    'gatsby-remark-copy-linked-files',
+                    'gatsby-remark-smartypants',
+                    {
+                        resolve: 'gatsby-remark-video',
+                        options: {
+                            width: 600,
+                            height: 'auto',
+                            preload: 'auto',
+                            muted: true,
+                            autoplay: true,
+                            playsinline: true,
+                            controls: false,
+                            loop: true
+                        }
+                    }
+                ]
+            }
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        'gatsby-plugin-netlify',
+        {
+            resolve: 'gatsby-plugin-netlify-cms',
+            options: {
+                modulePath: `${__dirname}/src/cms/index.js`
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-google-gtag',
+            options: {
+                trackingIds: [siteConfig.googleAnalyticsId],
+                pluginConfig: {
+                    head: true
+                }
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-sitemap',
+            options: {
+                query: `
           {
             site {
               siteMetadata {
@@ -163,30 +159,30 @@ module.exports = {
             }
           }
         `,
-        output: '/sitemap.xml',
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map(edge => ({
-            url: site.siteMetadata.siteUrl + edge.node.path,
-            changefreq: 'daily',
-            priority: 0.7
-          }))
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: siteConfig.title,
-        short_name: siteConfig.title,
-        start_url: '/',
-        background_color: '#FFF',
-        theme_color: '#97266d',
-        display: 'standalone',
-        icon: 'static/photo.jpg'
-      }
-    },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-optimize-svgs`
-  ]
+                output: '/sitemap.xml',
+                serialize: ({ site, allSitePage }) =>
+                    allSitePage.edges.map(edge => ({
+                        url: site.siteMetadata.siteUrl + edge.node.path,
+                        changefreq: 'daily',
+                        priority: 0.7
+                    }))
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: siteConfig.title,
+                short_name: siteConfig.title,
+                start_url: '/',
+                background_color: '#FFF',
+                theme_color: '#97266d',
+                display: 'standalone',
+                icon: 'static/photo.jpg'
+            }
+        },
+        'gatsby-plugin-catch-links',
+        'gatsby-plugin-react-helmet',
+        `gatsby-plugin-postcss`,
+        `gatsby-plugin-optimize-svgs`
+    ]
 }
